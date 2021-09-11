3a5a8343851e6f874e3a935f9a16eb2e9679da0a
Peter <43998116+PetercSnk@users.noreply.github.com>
2021-09-11 18:23:21 +0100

    only publishes True to Water topic

---
+++ manual_water.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+
+# for use until sensors are setup
+def onConnect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected OK")
+    else:
+        print("Bad connection, returned: ", rc)
+
+def onDisconnect(client, userdata, flags, rc = 0):
+    print("Disconnected, returned: ", str(rc))
+
+def onMesasge(client, userdata, message):
+    print("Recieved message: ", str(message.payload.decode("utf-8")))
+
+mqttBroker = "192.168.1.200"
+client = mqtt.Client("P")
+
+client.on_connect = onConnect
+client.on_disconnect = onDisconnect
+client.connect(mqttBroker)
+client.loop_start()
+client.publish("Water", True)
+client.loop_stop()
+client.disconnect(mqttBroker)
