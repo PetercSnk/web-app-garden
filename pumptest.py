a75f6bff927e216ea58690f5ac4cf4fa24b65f08
PetercSnk <peter.ch.snook@gmail.com>
2021-09-23 16:54:47 +0100

    pump works

---
+++ pumptest.py
@@ -0,0 +1,48 @@
+import time
+import grovepi
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def water_on(relay, switch):
+    grovepi.pinMode(relay, "OUTPUT")
+    grovepi.digitalWrite(relay, 1)
+    high = False
+    low = False
+    while True:
+        i = GPIO.input(switch)
+        print(i)
+        if (i == 0):
+            low = True
+            time.sleep(0.1)
+        if (i == 1):
+            high = True
+            time.sleep(0.1)
+        if high and not low:
+            high = False
+        if high and low:
+            grovepi.digitalWrite(relay, 0)
+            break
+
+def water_off(relay, switch):
+    grovepi.pinMode(relay, "OUTPUT")
+    grovepi.digitalWrite(relay, 1)
+    high = False
+    low = False
+    while True:
+        i = GPIO.input(switch)
+        print(i)
+        if (i == 1):
+            high = True
+            time.sleep(0.1)
+        if (i == 0):
+            low = True
+            time.sleep(0.1)
+        if low and not high:
+            low = False
+        if high and low:
+            grovepi.digitalWrite(relay, 0)
+            break
+
+water_on(3, 12)
