c5a60b63d040a2972b5f4d9a7a1003008ef68242
PetercSnk <peter.ch.snook@gmail.com>
2023-06-10 18:15:08 +0100

    slight change to pumptest

--- autorun.sh
+++
@@ -1,3 +0,0 @@
-screen -d -m -S GardenPi
-screen -S GardenPi -X stuff "cd ~Documents/repo/Automated-Garden"$(echo -ne "\015")
-screen -S GardenPi -X stuff "python pi.py"$(echo -ne "\015")
--- pumptest.py
+++ pumptest.py
@@ -1,50 +1,49 @@
 import time
-import grovepi
 import RPi.GPIO as GPIO
 
+valve_relay = 18
+valve_switch = 12
+
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(valve_switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(valve_relay, GPIO.OUT)
 
 def water_on(relay, switch):
-    grovepi.pinMode(relay, "OUTPUT")
-    grovepi.digitalWrite(relay, 1)
     high = False
     low = False
+    GPIO.output(relay, True)
     while True:
         i = GPIO.input(switch)
-        print(i)
         if (i == 0):
             low = True
             time.sleep(0.1)
-        if (i == 1):
+        elif (i == 1):
             high = True
             time.sleep(0.1)
         if high and not low:
             high = False
-        if high and low:
-            grovepi.digitalWrite(relay, 0)
+        elif high and low:
+            GPIO.output(relay, False)
             break
 
 def water_off(relay, switch):
-    grovepi.pinMode(relay, "OUTPUT")
-    grovepi.digitalWrite(relay, 1)
     high = False
     low = False
+    GPIO.output(relay, True)
     while True:
         i = GPIO.input(switch)
-        print(i)
-        if (i == 1):
-            high = True
-            time.sleep(0.1)
         if (i == 0):
             low = True
             time.sleep(0.1)
+        elif (i == 1):
+            high = True
+            time.sleep(0.1)
         if low and not high:
             low = False
-        if high and low:
-            grovepi.digitalWrite(relay, 0)
+        elif high and low:
+            GPIO.output(relay, False)
             break
 
-#water_on(3, 12)
-#time.sleep(5)
-water_off(3, 12)
+water_on(valve_relay, valve_switch)
+time.sleep(2)
+water_off(valve_relay, valve_switch)
