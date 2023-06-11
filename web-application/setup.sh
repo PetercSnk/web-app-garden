ffcf62aa8c6ad5dcc47a3d2944b4401c02d52866
PetercSnk <peter.ch.snook@gmail.com>
2023-06-11 12:27:02 +0100

    mv

--- web-application/setup.sh
+++ web-application/setup.sh
@@ -2,6 +2,6 @@ sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-mv automated-garden.service /etc/systemd/system/
+sudo mv automated-garden.service /etc/systemd/system/
 sudo systemctl enable automated-garden
 sudo systemctl start automated-garden
