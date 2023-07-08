1ee054a8f7d018c8939f23ab646e1a0d52c03387
PetercSnk <peter.ch.snook@gmail.com>
2023-07-08 14:27:48 +0100

    resets valve and pump on restart

--- web-application/app/__init__.py
+++ web-application/app/__init__.py
@@ -11,24 +11,24 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    from .models import db
+    from .models import db, User, WaterStatus
     db.init_app(app)
 
     executor.init_app(app)
-
     scheduler.init_app(app)
     scheduler.start()
 
     from .routes import routes
     from .auth import auth
-
     app.register_blueprint(routes, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
-
-    from .models import User
     
     with app.app_context():
         db.create_all()
+        # incase of restart during water
+        water_status = WaterStatus.query.first()
+        water_status.status = False
+        db.session.commit()
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -40,5 +40,16 @@ def create_app():
     
     from . import commands
     app.cli.add_command(commands.create_user)
+
+    # incase of restart during water
+    from .pump import Pump
+    from .valve import Valve
+    pump_relay = 16
+    valve_relay = 18
+    valve_switch = 12
+    valve = Valve(valve_relay, valve_switch)
+    pump = Pump(pump_relay)
+    valve.valve_off()
+    pump.pump_off()
 
     return app
