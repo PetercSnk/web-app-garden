7545434c625cd897ffb7e0711be76aa1ce984db9
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-07 16:55:23 +0100

    changed static path

--- web-application-bp/app/water/__init__.py
+++ web-application-bp/app/water/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-water_bp = Blueprint("water_bp", __name__, template_folder="templates", static_folder="core/static")
+water_bp = Blueprint("water_bp", __name__, template_folder="templates", static_folder="static")
 
 from app.water import water
--- web-application-bp/app/weather/__init__.py
+++ web-application-bp/app/weather/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-weather_bp = Blueprint("weather_bp", __name__, template_folder="templates", static_folder="base/static")
+weather_bp = Blueprint("weather_bp", __name__, template_folder="templates", static_folder="static")
 
 from app.weather import weather_bp
