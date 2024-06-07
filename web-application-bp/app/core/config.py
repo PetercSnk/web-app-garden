65addd262e32124824553edba9f12cc393f17751
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-07 16:54:45 +0100

    moved database locations

--- web-application-bp/app/core/config.py
+++ web-application-bp/app/core/config.py
@@ -1,5 +1,15 @@
+import os
+
+basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
 class Config(object):
     SECRET_KEY = "GardenPi"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "core", "instance", "default.db")
+    SQLALCHEMY_BINDS = {
+        "users": "sqlite:///" + os.path.join(basedir, "auth", "instance", "users.db"),
+        "weather": "sqlite:///" + os.path.join(basedir, "weather", "instance", "weather.db"),
+        "water": "sqlite:///" + os.path.join(basedir, "water", "instance", "water.db")
+    }
     EXECUTOR_TYPE = "thread"
     SCHEDULER_API_ENABLED = True
