e2ee2226d79a95bfeb581d78576cead33fc9e315
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-11 18:12:45 +0100

    hide graph when no day data available

--- web-application-bp/app/weather/weather.py
+++ web-application-bp/app/weather/weather.py
@@ -6,43 +6,43 @@ from app.core import jobs
 from app.weather import weather_bp
 
 
-@weather_bp.route("/", methods=["GET"])
+@weather_bp.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     today = datetime.now().date()
-    db_today = Day.query.filter(Day.date == today).one()
+    db_today = Day.query.filter(Day.date == today).first()
     if db_today:
         # redirect to todays id if it exists in db
-        return redirect(url_for("weather.weather", day_id=db_today.id))
+        return redirect(url_for("weather_bp.graph", day_id=db_today.id))
     else:
         db_first = Day.query.first()
         if db_first:
             # redirect to some other id that exists if today is not in db
-            return redirect(url_for("weather.weather", day_id=db_first.id))
+            return redirect(url_for("weather_bp.graph", day_id=db_first.id))
         else:
-            # if nothing exists in db redirect to /0
-            return redirect(url_for("weather.weather", day_id=0))
+            # if nothing exists in db redirect with day_id as None
+            return render_template("weather/weather.html", render=False, user=current_user)
 
 
 @weather_bp.route("/<int:day_id>", methods=["GET", "POST"])
 @login_required
-def weather(day_id):
+def graph(day_id):
     if request.method == "POST":
         if "get-weather" in request.form:
             msg = jobs.get_weather()
             flash(msg, category="info")
-            return redirect(url_for("weather.weather", day_id=day_id))
-    else:
+            return redirect(url_for("weather_bp.index"))
+    elif request.method == "GET":
         db_days = Day.query.order_by(Day.date).all()
-        if day_id > 0:
-            date, sunrise, sunset, labels, temperature_c, humidity, rain_probability, rain_volume_mm = format_for_graph(day_id)
-            return render_template("weather.html", user=current_user, db_days=db_days, date=date, sunrise=sunrise, sunset=sunset, labels=labels, temperature_c=temperature_c, humidity=humidity, rain_probability=rain_probability, rain_volume_mm=rain_volume_mm)
+        if day_id in [db_day.id for db_day in db_days]:
+            date, sunrise, sunset, labels, temperature_c, humidity, rain_probability, rain_volume_mm = format(day_id)
+            return render_template("weather/weather.html", render=True, user=current_user, db_days=db_days, date=date, sunrise=sunrise, sunset=sunset, labels=labels, temperature_c=temperature_c, humidity=humidity, rain_probability=rain_probability, rain_volume_mm=rain_volume_mm)
         else:
-            return render_template("weather.html", user=current_user, db_days=db_days, date=None, sunrise=None, sunset=None, labels=0, temperature_c=0, humidity=0, rain_probability=0, rain_volume_mm=0)
+            return render_template("weather/weather.html", render=False, user=current_user)
 
 
-def format_for_graph(day_id):
-    day = Day.query.filter(Day.id == day_id).one()
+def format(day_id):
+    day = Day.query.filter(Day.id == day_id).first()
     weather = Weather.query.filter(Weather.day_id == day.id).order_by(Weather.time).all()
     labels, temperature_c, humidity, rain_probability, rain_volume_mm = [], [], [], [], []
     for three_hour_step in weather:
