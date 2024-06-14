d15dd8deafa7b2dd8304abb3ec8e90dc8d328219
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-14 16:39:54 +0100

    redirects to weather if authenticated else login

--- web-application-bp/app/core/core.py
+++ web-application-bp/app/core/core.py
@@ -1,7 +1,11 @@
 from flask import redirect, url_for
+from flask_login import current_user
 from app.core import core_bp
 
 
 @core_bp.route("/", methods=["GET"])
 def core_index():
-    return redirect(url_for("auth_bp.login"))
+    if current_user.is_authenticated:
+        return redirect(url_for("weather_bp.index"))
+    else:
+        return redirect(url_for("auth_bp.login"))
