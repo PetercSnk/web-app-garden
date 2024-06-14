f51f7e4461d5732b5e1e14a416a2e30b87e360de
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-14 12:42:14 +0100

    moved create user command to auth

---
+++ web-application-bp/app/auth/commands.py
@@ -0,0 +1,14 @@
+from werkzeug.security import generate_password_hash
+from app import db
+from app.auth.models import User
+import click
+
+
+@click.command()
+@click.argument("username")
+@click.argument("password")
+def create_user(username, password):
+    """Create user account."""
+    user = User(username=username, password=generate_password_hash(password, method="sha256"))
+    db.session.add(user)
+    db.session.commit()
