e1375d0bbf2190fee6be0014227dc86d0a901e13
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-14 12:43:02 +0100

    more readable layout

--- web-application-bp/app/__init__.py
+++ web-application-bp/app/__init__.py
@@ -7,59 +7,62 @@ db = SQLAlchemy()
 
 
 def create_app():
-    # create app instance
+    # create and configure flask instance
     app = Flask(__name__)
-    # load config from obj
     from app.core.config import Config
     app.config.from_object(Config)
-    # register blueprints
-    from app.core import core_bp
-    from app.auth import auth_bp
-    from app.water import water_bp
-    from app.weather import weather_bp
-    app.register_blueprint(core_bp)
-    app.register_blueprint(auth_bp, url_prefix="/auth")
-    app.register_blueprint(water_bp, url_prefix="/water")
-    app.register_blueprint(weather_bp, url_prefix="/weather")
-    # initialise executor and scheduler
+
+    # initialise databases
+    db.init_app(app)
+
+    # initialise extensions
     from app.core import extensions
     extensions.executor.init_app(app)
     extensions.scheduler.init_app(app)
     extensions.scheduler.start()
-    # import jobs so tasks execute
-    from app.weather import jobs
-    # create database instance, import User for user loader
-    db.init_app(app)
-    # create login manager instance
+
+    # core module setup
+    from app.core import core_bp
+    app.register_blueprint(core_bp)
+    from app.core import commands as core_cmds
+    app.cli.add_command(core_cmds.init_db)
+    app.cli.add_command(core_cmds.drop_db)
+
+    # auth module setup
+    from app.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    from app.auth import commands as auth_cmds
+    app.cli.add_command(auth_cmds.create_user)
+    from app.auth.models import User
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
-
-    # required to check user is authenticated
-    from app.auth.models import User
 
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
-    # create tables based on models
-    from app.water.models import WaterStatus
-    with app.app_context():
-        db.create_all()
-        # REPLACE WITH JUST A VARIABLE AND IMPORT?? EASIER THAN DB
-        water_status = WaterStatus.query.first()
-        if not water_status:
-            db.session.add(WaterStatus(status=False))
-            db.session.commit()
-        # incase of restart during water
-        water_status = WaterStatus.query.one()
-        water_status.status = False
-        db.session.commit()
+    # weather module setup
+    from app.weather import weather_bp, jobs
+    app.register_blueprint(weather_bp, url_prefix="/weather")
+
+    # water module setup
+    from app.water import water_bp
+    app.register_blueprint(water_bp, url_prefix="/water")
+    water_status = False
+    # from app.water.models import WaterStatus
+    #     # REPLACE WITH JUST A VARIABLE AND IMPORT?? EASIER THAN DB
+    #     water_status = WaterStatus.query.first()
+    #     if not water_status:
+    #         db.session.add(WaterStatus(status=False))
+    #         db.session.commit()
+    #     # incase of restart during water
+    #     water_status = WaterStatus.query.one()
+    #     water_status.status = False
+    #     db.session.commit()
 
     # command for creating a user account
     # MAKE THIS A REGISTER PAGE INSTEAD OF COMMAND
-    from app.core import commands
-    app.cli.add_command(commands.create_user)
 
     # from .pump import Pump
     # from .valve import Valve
