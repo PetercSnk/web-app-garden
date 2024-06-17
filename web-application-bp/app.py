339c78b73d71130d7ba70cf777e40170949b1bdf
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-17 16:45:03 +0100

    unsure how to use normal logging & gunicorn

--- web-application-bp/app.py
+++ web-application-bp/app.py
@@ -4,8 +4,9 @@ import logging
 app = create_app()
 
 if __name__ == "__main__":
+    #logging.basicConfig(level=logging.DEBUG, format="[%(asctime)s] %(levelname)s | %(name)s %(threadName)s >>> %(message)s")
     app.run(host="0.0.0.0")
 else:
-    gunicorn_logger = logging.getLogger('gunicorn.error')
+    gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
