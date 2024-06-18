89e0f0e2fb3cd6bece90cf14a8d817e1dc14cb4e
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-18 16:59:30 +0100

    split water into seperate funcs, fix required

--- web-application-bp/app/water/water.py
+++ web-application-bp/app/water/water.py
@@ -15,21 +15,21 @@ from app.water import water_bp
 def index():
     water_status = WaterStatus.query.first()
     if request.method == "POST":
-        if "wtime" in request.form:
+        if "water" in request.form:
             if water_status.status:
                 flash("Already Runnning", category="error")
             else:
-                water_time = int(request.form.get("wtime"))
-                water_status.status = True
-                db.session.add(Water(start_date_time=datetime.now(), duration=water_time))
-                db.session.commit()
-                water.submit(water_time)
+                water_time = int(request.form.get("water"))
+                # water_status.status = True
+                # db.session.add(Water(start_date_time=datetime.now(), duration=water_time))
+                # db.session.commit()
+                water.submit(water_status, water_time)
                 flash("Started Process", category="success")
-        elif "fstop" in request.form:
+        elif "stop" in request.form:
             if water_status.status:
                 event.set()
-                water_status.status = False
-                db.session.commit()
+                # water_status.status = False
+                # db.session.commit()
                 flash("Stopped Process", category="error")
             else:
                 flash("Not Running", category="error")
@@ -37,21 +37,31 @@ def index():
 
 
 @executor.job
-def water(water_time):
+def water(water_status, water_time):
+    water_status.status = True
+    db.session.add(Water(start_date_time=datetime.now(), duration=water_time))
+    db.session.commit()
+    executor.app.logger.info(f"Watering for {water_time} seconds")
+    print("START")
+    loop(water_time)
     # valve = Valve()
     # pump = Pump()
     # valve.valve_on()
     # pump.pump_on()
+    # valve.valve_off()
+    # pump.pump_off()
+    print("STOP")
+    water_status.status = False
+    db.session.commit()
+    executor.app.logger.info("Stopped watering process")
+    return
+
+
+def loop(water_time):
     for x in range(water_time):
         time.sleep(1)
         if event.is_set():
-            # valve.valve_off()
-            # pump.pump_off()
+            print("EVENT")
+            executor.app.logger.info("Event set")
             event.clear()
             return
-    # valve.valve_off()
-    # pump.pump_off()
-    water_status = WaterStatus.query.first()
-    water_status.status = False
-    db.session.commit()
-    return
