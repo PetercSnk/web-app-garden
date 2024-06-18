927845f16612576f452d02399beeda88a11e60d5
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-18 17:00:02 +0100

    error handling for drop day

--- web-application-bp/app/weather/commands.py
+++ web-application-bp/app/weather/commands.py
@@ -8,5 +8,8 @@ import click
 def drop_day(day_id):
     """Delete specific day entry by id."""
     day = Day.query.filter_by(id=day_id).first()
-    db.session.delete(day)
-    db.session.commit()
+    if day:
+        db.session.delete(day)
+        db.session.commit()
+    else:
+        print(f"Day with id '{day_id}' does not exist")
