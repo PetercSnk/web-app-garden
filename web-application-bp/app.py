b04ffef11ed4f85a4d1f25dd196641493a095be7
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-20 12:21:49 +0100

    added todo comment

--- web-application-bp/app.py
+++ web-application-bp/app.py
@@ -6,6 +6,7 @@ app = create_app()
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 else:
+    # TODO test gunicorn with linux
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
