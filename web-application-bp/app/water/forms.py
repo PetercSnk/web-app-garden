4ee6c370204b24e9272f3753953352f56cb80df7
Peter Snook <snookp2@cardiff.ac.uk>
2024-06-25 16:19:59 +0100

    new auto form and model

--- web-application-bp/app/water/forms.py
+++ web-application-bp/app/water/forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
+from wtforms import IntegerField, TimeField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
 
 class WaterForm(FlaskForm):
-    duration = IntegerField("Time (Seconds)", validators=[DataRequired(), NumberRange(min=30, max=300, message="Minimum: %(min)s, Maximum: %(max)s")])
+    duration_sec = IntegerField("Time (Seconds)", validators=[DataRequired(), NumberRange(min=30, max=300, message="Minimum: %(min)s, Maximum: %(max)s")])
     submit = SubmitField("Water")
 
 
 class CancelForm(FlaskForm):
     submit = SubmitField("Cancel")
+
+
+class AutoForm(FlaskForm):
+    duration_sec = IntegerField("Water Duration (Seconds)", validators=[DataRequired(), NumberRange(min=30, max=300, message="Minimum: %(min)s, Maximum: %(max)s")])
+    min_wait_hr = IntegerField("Minimum Wait (Hours)", validators=[DataRequired()])
+    mode = IntegerField("Mode (1:Sunset, 2:Sunrise, 3:Custom)", validators=[DataRequired(), NumberRange(min=1, max=3, message="Modes only 1, 2, and 3 are available")])
+    time = TimeField("Custom Water Time", validators=[DataRequired()])
+    rain_reset = BooleanField("Reset Timer on Rainfall", validators=[DataRequired()])
--- web-application-bp/app/water/models.py
+++ web-application-bp/app/water/models.py
@@ -5,7 +5,7 @@ class Water(db.Model):
     __bind_key__ = "water"
     id = db.Column(db.Integer, primary_key=True)
     start_date_time = db.Column(db.DateTime)
-    duration = db.Column(db.Integer)
+    duration_sec = db.Column(db.Integer)
 
     def __repr__(self):
         return f"<Water: {self.start_date_time}>"
@@ -18,3 +18,16 @@ class WaterStatus(db.Model):
 
     def __repr__(self):
         return f"<WaterStatus: {self.status}>"
+
+
+class WaterAuto(db.Model):
+    __bind_key__ = "water"
+    id = db.Column(db.Integer, primary_key=True)
+    duration_sec = db.Column(db.Integer)
+    min_wait_hr = db.Column(db.Integer)
+    mode = db.Column(db.Integer)
+    time = db.Column(db.Time)
+    rain_reset = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f"<WaterAuto: {self.id}"
