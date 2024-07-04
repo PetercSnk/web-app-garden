188f59b376b512d8e1b7ba11d4a191919ea8d39e
Peter Snook <snookp2@cardiff.ac.uk>
2024-07-04 16:56:49 +0100

    overhaul of water module 1.3
    fix threading for water function, doesnt seem to update status

--- web-application-bp/app/water/forms.py
+++ web-application-bp/app/water/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TimeField, BooleanField, SelectField, SubmitField
+from wtforms import StringField, IntegerField, TimeField, BooleanField, SelectField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, Length, NumberRange
 
 
@@ -7,16 +7,12 @@ class PlantForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(max=10)])
     description = StringField("Description", validators=[DataRequired(), Length(max=50)])
     system = SelectField("System", coerce=int, validators=[DataRequired()])
-    submit = SubmitField("Save")
+    submit = SubmitField("Create")
 
 
 class WaterForm(FlaskForm):
     duration_sec = IntegerField("Time (Seconds)", validators=[DataRequired(), NumberRange(min=30, max=300, message="Minimum: %(min)s, Maximum: %(max)s")])
     submit = SubmitField("Water")
-
-
-class CancelForm(FlaskForm):
-    submit = SubmitField("Cancel")
 
 
 class ConfigForm(FlaskForm):
--- web-application-bp/app/water/water.py
+++ web-application-bp/app/water/water.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, current_app, redirect, url_for, reques
 from flask_login import login_required, current_user
 from app.core.extensions import event
 from app.water.models import History, Config, Plant, System
-from app.water.forms import WaterForm, CancelForm, ConfigForm, PlantForm
+from app.water.forms import WaterForm, ConfigForm, PlantForm
 from app.water import water_bp
 from app import db
 import threading
@@ -10,22 +10,46 @@ from datetime import datetime
 import time
 
 
+@water_bp.route("/delete/<int:plant_id>", methods=["GET", "POST"])
+@login_required
+def delete(plant_id):
+    """Delete plants & all relationships"""
+    plant = Plant.query.filter(Plant.id == plant_id).first()
+    if plant:
+        flash("Deleted plant", category="success")
+        db.session.delete(plant)
+        db.session.commit()
+    else:
+        flash("Does not exist", category="error")
+    return redirect(url_for("water_bp.setup"))
+
+
 @water_bp.route("/setup", methods=["GET", "POST"])
 @login_required
 def setup():
+    """Create plants & auto assign a default config"""
     plant_form = PlantForm()
     systems_available = [(system.id, system.name) for system in System.query.all()]
     plant_form.system.choices = systems_available
     if request.method == "POST" and plant_form.validate():
-        plant = Plant(name=plant_form.name.data,
-                      description=plant_form.description.data,
-                      status=False)
-        system = System.query.filter(System.id == plant_form.system.data)
-        system.plants.append(plant)
+        new_plant = Plant(name=plant_form.name.data,
+                          description=plant_form.description.data,
+                          status=False,
+                          system_id=plant_form.system.data)
+        new_config = Config(enabled=False,
+                            duration_sec=120,
+                            min_wait_hr=24,
+                            mode=3,
+                            default=datetime.strptime("6", "%H").time(),
+                            rain_reset=False)
+        new_plant.config.append(new_config)
+        db.session.add(new_plant)
         db.session.commit()
+    plants_available = Plant.query.all()
     return render_template("water/setup.html",
                            user=current_user,
-                           plant_form=plant_form)
+                           plant_form=plant_form,
+                           plants_available=plants_available)
 
 
 # @water_bp.route("/configure", methods=["GET"])
@@ -72,9 +96,28 @@ def setup():
 #                            plant=plant)
 #
 
-@water_bp.route("/water", methods=["GET"])
+@water_bp.route("/cancel/<int:plant_id>", methods=["GET", "POST"])
 @login_required
-def water_index():
+def cancel(plant_id):
+    """Cancel watering process in thread created by water"""
+    plant_selected = Plant.query.filter(Plant.id == plant_id).first()
+    if plant_selected:
+        if plant_selected.status:
+            event.set()
+            current_app.logger.debug("Event set")
+            flash("Stopped Process", category="success")
+        else:
+            flash("Not Running", category="error")
+        return redirect(url_for("water_bp.water", plant_id=plant_id))
+    else:
+        flash("Does not exist", category="error")
+        return redirect(url_for("water_bp.water_check"))
+
+
+@water_bp.route("/check", methods=["GET"])
+@login_required
+def water_check():
+    """Check if plants exist and redirect accordingly"""
     plant = Plant.query.first()
     if plant:
         return redirect(url_for("water_bp.water", plant_id=plant.id))
@@ -86,52 +129,36 @@ def water_index():
 @water_bp.route("/water/<int:plant_id>", methods=["GET", "POST"])
 @login_required
 def water(plant_id):
-    plants = Plant.query.all()
-    plant = Plant.query.filter(Plant.id == plant_id)
-    water_form = WaterForm()
-    cancel_form = CancelForm()
-    if water_form.submit.data and water_form.validate():
-        if plant.status:
-            flash("Already Runnning", category="error")
-        else:
-            duration_sec = water_form.duration_sec.data
-            thread = threading.Thread(target=process,
-                                      args=(current_app._get_current_object(), duration_sec, plant),
-                                      daemon=True)
-            thread.start()
-            current_app.logger.debug("Started thread")
-            flash("Started Process", category="success")
-            return render_template("water/water.html",
-                                   user=current_user,
-                                   status=True,
-                                   water_form=water_form,
-                                   cancel_form=cancel_form,
-                                   plants=plants)
-    elif cancel_form.submit.data and cancel_form.validate():
-        if plant.status:
-            event.set()
-            current_app.logger.debug("Event set")
-            flash("Stopped Process", category="success")
-            return render_template("water/water.html",
-                                   user=current_user,
-                                   status=False,
-                                   water_form=water_form,
-                                   cancel_form=cancel_form,
-                                   plants=plants)
-        else:
-            flash("Not Running", category="error")
-    return render_template("water/water.html",
-                           user=current_user,
-                           status=plant.status,
-                           water_form=water_form,
-                           cancel_form=cancel_form,
-                           plants=plants)
+    """Create new thread and start watering process"""
+    plant_selected = Plant.query.filter(Plant.id == plant_id).first()
+    if plant_selected:
+        water_form = WaterForm()
+        plants_available = Plant.query.all()
+        if request.method == "POST" and water_form.validate():
+            if plant_selected.status:
+                flash("Already Runnning", category="error")
+            else:
+                thread = threading.Thread(target=process,
+                                          args=(current_app._get_current_object(), water_form.duration_sec.data, plant_selected),
+                                          daemon=True)
+                thread.start()
+                current_app.logger.debug("Started thread")
+                flash("Started Process", category="success")
+        return render_template("water/water.html",
+                               user=current_user,
+                               plant_selected=plant_selected,
+                               water_form=water_form,
+                               plants_available=plants_available)
+    else:
+        flash("Does not exist", category="error")
+        return redirect(url_for("water_bp.water_check"))
 
 
 def process(app, duration_sec, plant):
+    """Watering process"""
     with app.app_context():
         plant.status = True
-        plant.history.append(History(start_date_time=datetime.now(), duration_sec=duration_sec))
+        #plant.history.append(History(start_date_time=datetime.now(), duration_sec=duration_sec))
         db.session.commit()
         app.logger.debug(f"Plant status set to {plant.status}")
         app.logger.info(f"Watering for {duration_sec} seconds")
@@ -146,6 +173,7 @@ def process(app, duration_sec, plant):
 
 
 def loop(app, duration_sec):
+    """Inner loop of watering process"""
     for x in range(duration_sec):
         time.sleep(1)
         if event.is_set():
