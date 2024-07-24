d6527a6f8b9f2ff12ffeafde7b8628346e68cfd6
Peter Snook <snookp2@cardiff.ac.uk>
2024-07-24 12:01:17 +0100

    get next estimate takes in config to avoid extra query

--- web-application-bp/app/water/jobs.py
+++ web-application-bp/app/water/jobs.py
@@ -1,5 +1,5 @@
 from app.water.models import Plant
-from app import scheduler
+from app import db, scheduler
 import pytz
 from datetime import datetime, timedelta
 from suntime import Sun
@@ -12,27 +12,26 @@ def get_sun_tz():
     return sun, tz
 
 
-def get_next_estimate(plant_id):
+def get_next_estimate(config):
     """Get the next earliest estimate for watering."""
-    plant_selected = Plant.query.filter(Plant.id == plant_id).first()
     today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    estimate = today + timedelta(days=plant_selected.config.occurrence_days)
-    if plant_selected.config.mode == 1:
+    estimate = today + timedelta(days=config.occurrence_days)
+    if config.mode == 1:
         sun, tz = get_sun_tz()
         sunset = sun.get_sunset_time(estimate, tz).time()
         estimate = estimate.replace(hour=sunset.hour, minute=sunset.minute, second=sunset.second)
-    elif plant_selected.config.mode == 2:
+    elif config.mode == 2:
         sun, tz = get_sun_tz()
         sunrise = sun.get_sunrise_time(estimate, tz).time()
         estimate = estimate.replace(hour=sunrise.hour, minute=sunrise.minute, second=sunrise.second)
     else:
-        default = plant_selected.config.default
+        default = config.default
         estimate = estimate.replace(hour=default.hour, minute=default.minute, second=default.second)
     return estimate
 
 
 # run on date of water, check weather if enabled, check sun, create job for water
-def rescheduler(plant_id):
+def add_job(plant_id):
     scheduler.app.logger.debug(f"run rescheduler for {plant_id}")
 
 
