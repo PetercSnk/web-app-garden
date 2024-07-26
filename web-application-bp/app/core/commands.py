6f40bc23008f41dc9fbefd84fd0ef5174175d451
Peter Snook <snookp2@cardiff.ac.uk>
2024-07-26 16:48:50 +0100

    moved create all to init

--- web-application-bp/app/core/commands.py
+++ web-application-bp/app/core/commands.py
@@ -3,12 +3,6 @@ import click
 
 
 @click.command()
-def init_db():
-    """Create databases."""
-    db.create_all()
-
-
-@click.command()
 def drop_db():
     """Delete databases."""
     db.drop_all()
