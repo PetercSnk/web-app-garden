1f1cce12c27a51238ba80e2fba508b171d419c85
Peter Snook <snookp2@cardiff.ac.uk>
2024-08-05 16:38:13 +0100

    moved description to day table & renamed other fields

--- web-application-bp/app/weather/models.py
+++ web-application-bp/app/weather/models.py
@@ -7,6 +7,7 @@ class Day(db.Model):
     date = db.Column(db.Date)
     sunrise = db.Column(db.Time)
     sunset = db.Column(db.Time)
+    description = db.Column(db.String(64))
     weather = db.relationship("Weather", backref="day", cascade="all, delete-orphan")
 
     def __repr__(self):
@@ -18,11 +19,10 @@ class Weather(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     day_id = db.Column(db.Integer, db.ForeignKey("day.id"))
     time = db.Column(db.Time)
-    temperature_c = db.Column(db.Integer)
+    temperature = db.Column(db.Integer)
     humidity = db.Column(db.Integer)
-    description = db.Column(db.String(64))
-    rain_probability = db.Column(db.Integer)
-    rain_volume_mm = db.Column(db.Integer)
+    precipitation_probability = db.Column(db.Integer)
+    precipitation = db.Column(db.Integer)
 
     def __repr__(self):
         return f"<Weather: {self.day_id}>"
