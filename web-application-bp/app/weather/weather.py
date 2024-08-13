19756545d7d24e437ad0c870cc33327cac0e2493
Peter Snook <snookp2@cardiff.ac.uk>
2024-08-13 16:43:34 +0100

    renamed and amended for new models

--- web-application-bp/app/weather/weather.py
+++ web-application-bp/app/weather/weather.py
@@ -8,52 +8,63 @@ from datetime import datetime
 @weather_bp.route("/", methods=["GET"])
 @login_required
 def index():
-    today = datetime.now().date()
-    day = Daily.query.filter(Daily.date == today).first()
-    if day:
+    daily_today = Daily.query.filter(Daily.date == datetime.now().date()).first()
+    if daily_today:
         # redirect to todays id if it exists in db
-        return redirect(url_for("weather_bp.graph", day_id=day.id))
+        return redirect(url_for("weather_bp.graph", daily_id=daily_today.id))
     else:
-        day = Daily.query.first()
-        if day:
+        daily_first = Daily.query.first()
+        if daily_first:
             # redirect to some other id that exists if today is not in db
-            return redirect(url_for("weather_bp.graph", day_id=day.id))
+            return redirect(url_for("weather_bp.graph", daily_id=daily_first.id))
         else:
-            # if nothing exists in db redirect with any value for day_id, the graph won't render regardless due to check in graph function
-            return redirect(url_for("weather_bp.graph", day_id=0))
+            # if nothing exists in db redirect with any value for daily_id, the graph won't render regardless due to check in graph function
+            return redirect(url_for("weather_bp.graph", daily_id=0))
 
 
-@weather_bp.route("/<int:day_id>", methods=["GET", "POST"])
+@weather_bp.route("/<int:daily_id>", methods=["GET", "POST"])
 @login_required
-def graph(day_id):
+def graph(daily_id):
     if request.method == "POST":
         if "get-weather" in request.form:
             current_app.logger.debug("Getting weather")
-            #msg = jobs.get_weather()
-            jobs.get_weather()
-            #flash(msg, category="info")
+            daily_count, hourly_count = jobs.get_weather()
+            msg = f"Added {daily_count} daily entries and {hourly_count} hourly entries."
+            flash(msg, category="info")
             return redirect(url_for("weather_bp.index"))
     elif request.method == "GET":
-        # days = Daily.query.order_by(Daily.date).all()
-        # if day_id in [day.id for day in days]:
-        #     date, sunrise, sunset, labels, temperature_c, humidity, rain_probability, rain_volume_mm = format(day_id)
-        #     return render_template("weather/weather.html", render=True, user=current_user, days=days, date=date, sunrise=sunrise, sunset=sunset, labels=labels, temperature_c=temperature_c, humidity=humidity, rain_probability=rain_probability, rain_volume_mm=rain_volume_mm)
-        # else:
-        #     flash("No Data", category="info")
+        daily_all = Daily.query.order_by(Daily.date).all()
+        if daily_id in [daily.id for daily in daily_all]:
+            daily = Daily.query.filter(Daily.id == daily_id).first()
+            time, temperature, humidity, precipitation_probability, precipitation = format(daily_id)
+            return render_template("weather/weather.html",
+                                   render=True,
+                                   user=current_user,
+                                   daily_all=daily_all,
+                                   date=daily.date,
+                                   sunrise=daily.sunrise,
+                                   sunset=daily.sunset,
+                                   description=daily.weather_description,
+                                   time=time,
+                                   temperature=temperature,
+                                   humidity=humidity,
+                                   precipitation_probability=precipitation_probability,
+                                   precipitation=precipitation)
+        else:
+            flash("No Data", category="info")
         return render_template("weather/weather.html", render=False, user=current_user)
 
 
-def format(day_id):
-    day = Daily.query.filter(Daily.id == day_id).first()
-    weather = Hourly.query.filter(Hourly.day_id == day.id).order_by(Hourly.time).all()
-    labels, temperature_c, humidity, rain_probability, rain_volume_mm = [], [], [], [], []
-    for three_hour_step in weather:
-        labels.append(f"{three_hour_step.time.strftime('%H:%M')} {three_hour_step.description.title()}")
-        temperature_c.append(three_hour_step.temperature_c)
-        humidity.append(three_hour_step.humidity)
-        rain_probability.append(three_hour_step.rain_probability)
-        rain_volume_mm.append(three_hour_step.rain_volume_mm)
-    return day.date, day.sunrise, day.sunset, labels, temperature_c, humidity, rain_probability, rain_volume_mm
+def format(daily_id):
+    hourly_all = Hourly.query.filter(Hourly.daily_id == daily_id).order_by(Hourly.time).all()
+    time, temperature, humidity, precipitation_probability, precipitation = [], [], [], [], []
+    for hourly in hourly_all:
+        time.append(f"{hourly.time.strftime('%H:%M')}")
+        temperature.append(hourly.temperature)
+        humidity.append(hourly.humidity)
+        precipitation_probability.append(hourly.precipitation_probability)
+        precipitation.append(hourly.precipitation)
+    return time, temperature, humidity, precipitation_probability, precipitation
 
 
 @weather_bp.context_processor
