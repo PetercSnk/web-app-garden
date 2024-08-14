fb3e2ff6221c9e65e37277b9952f13dc4f450b27
Peter Snook <snookp2@cardiff.ac.uk>
2024-08-14 16:51:12 +0100

    added docstrings and cleaned up formatting

--- web-application-bp/app/weather/weather.py
+++ web-application-bp/app/weather/weather.py
@@ -1,30 +1,33 @@
+"""Routes for the weather module."""
+from datetime import datetime
 from flask import render_template, request, flash, redirect, url_for, current_app
 from flask_login import login_required, current_user
 from app.weather.models import Daily, Hourly
 from app.weather import weather_bp, jobs
-from datetime import datetime
 
 
 @weather_bp.route("/", methods=["GET"])
 @login_required
 def index():
+    """Checks what daily records exist and redirects accordingly.
+
+    Will redirect to either todays date, the first available record, or nothing.
+    """
     daily_today = Daily.query.filter(Daily.date == datetime.now().date()).first()
     if daily_today:
-        # redirect to todays id if it exists in db
         return redirect(url_for("weather_bp.graph", daily_id=daily_today.id))
     else:
         daily_first = Daily.query.first()
         if daily_first:
-            # redirect to some other id that exists if today is not in db
             return redirect(url_for("weather_bp.graph", daily_id=daily_first.id))
         else:
-            # if nothing exists in db redirect with any value for daily_id, the graph won't render regardless due to check in graph function
             return redirect(url_for("weather_bp.graph", daily_id=0))
 
 
 @weather_bp.route("/<int:daily_id>", methods=["GET", "POST"])
 @login_required
 def graph(daily_id):
+    """Handles daily weather charts and requests for weather data."""
     if request.method == "POST":
         if "get-weather" in request.form:
             current_app.logger.debug("Getting weather")
@@ -33,42 +36,46 @@ def graph(daily_id):
             flash(msg, category="info")
             return redirect(url_for("weather_bp.index"))
     elif request.method == "GET":
-        daily_all = Daily.query.order_by(Daily.date).all()
-        if daily_id in [daily.id for daily in daily_all]:
-            daily = Daily.query.filter(Daily.id == daily_id).first()
-            time, temperature, humidity, precipitation_probability, precipitation = format(daily_id)
+        available = Daily.query.order_by(Daily.date).all()
+        if daily_id in [record.id for record in available]:
+            daily_data = Daily.query.filter(Daily.id == daily_id).first()
+            hourly_data = get_formatted_hourly(daily_id)
             return render_template("weather/weather.html",
                                    render=True,
                                    user=current_user,
-                                   daily_all=daily_all,
-                                   date=daily.date,
-                                   sunrise=daily.sunrise,
-                                   sunset=daily.sunset,
-                                   description=daily.weather_description,
-                                   time=time,
-                                   temperature=temperature,
-                                   humidity=humidity,
-                                   precipitation_probability=precipitation_probability,
-                                   precipitation=precipitation)
+                                   available=available,
+                                   city=current_app.config["CITY"],
+                                   region=current_app.config["REGION"],
+                                   daily_data=daily_data,
+                                   hourly_data=hourly_data)
         else:
             flash("No Data", category="info")
         return render_template("weather/weather.html", render=False, user=current_user)
 
 
-def format(daily_id):
-    hourly_all = Hourly.query.filter(Hourly.daily_id == daily_id).order_by(Hourly.time).all()
-    time, temperature, humidity, precipitation_probability, precipitation = [], [], [], [], []
-    for hourly in hourly_all:
-        time.append(f"{hourly.time.strftime('%H:%M')}")
-        temperature.append(hourly.temperature)
-        humidity.append(hourly.humidity)
-        precipitation_probability.append(hourly.precipitation_probability)
-        precipitation.append(hourly.precipitation)
-    return time, temperature, humidity, precipitation_probability, precipitation
+def get_formatted_hourly(daily_id):
+    """Retieves and formats all hourly data for the given daily id."""
+    hourly = Hourly.query.filter(Hourly.daily_id == daily_id).order_by(Hourly.time).all()
+    data = {
+        "time": [],
+        "temperature": [],
+        "humidity": [],
+        "precipitation_probability": [],
+        "precipitation": []
+    }
+    for record in hourly:
+        data["time"].append(f"{record.time.strftime('%H:%M')}")
+        data["temperature"].append(record.temperature)
+        data["humidity"].append(record.humidity)
+        data["precipitation_probability"].append(record.precipitation_probability)
+        data["precipitation"].append(record.precipitation)
+    return data
 
 
 @weather_bp.context_processor
 def utility():
+    """Context processors for use within html templates."""
     def format_date(datetime_obj):
+        """Formats datetime objects into readable format."""
         return datetime_obj.strftime("%d-%b")
     return dict(format_date=format_date)
