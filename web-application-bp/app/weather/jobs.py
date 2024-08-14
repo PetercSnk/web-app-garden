bb7463d28d6e532c0fd5c9041dc8c784007cbe7e
Peter Snook <snookp2@cardiff.ac.uk>
2024-08-14 12:22:00 +0100

    added function to remove anomalies

--- web-application-bp/app/weather/jobs.py
+++ web-application-bp/app/weather/jobs.py
@@ -1,14 +1,10 @@
 """All functions required for scheduling the retrieval of weather data."""
-from datetime import datetime
 import os
 import yaml
-import requests
-import traceback
 import pytz
 import openmeteo_requests
 import pandas as pd
 import numpy as np
-from suntime import Sun
 from astral import LocationInfo
 from astral.sun import sun
 from app.weather.models import Daily, Hourly
@@ -21,8 +17,10 @@ def get_weather():
     daily_dataframe, hourly_dataframe = format_response(response)
     daily_dataframe = insert_descriptions(daily_dataframe)
     daily_dataframe = insert_suntimes(daily_dataframe)
+    daily_dataframe, hourly_dataframe = delete_anomalies(daily_dataframe, hourly_dataframe)
     daily_count, hourly_count = insert_into_db(daily_dataframe, hourly_dataframe)
     return daily_count, hourly_count
+
 
 def get_response():
     url = "https://api.open-meteo.com/v1/forecast"
@@ -77,6 +75,16 @@ def format_response(response):
         "weather_code": daily_weather_code
     }
     daily_dataframe = pd.DataFrame(data=daily_data)
+    return daily_dataframe, hourly_dataframe
+
+
+def delete_anomalies(daily_dataframe, hourly_dataframe):
+    dates = daily_dataframe["date"].tolist()
+    for date in dates:
+        hourly_series = hourly_dataframe.loc[hourly_dataframe["date"] == date]
+        if hourly_series.size < 24:
+            hourly_dataframe = hourly_dataframe[hourly_dataframe.date != date]
+            daily_dataframe = daily_dataframe[daily_dataframe.date != date]
     return daily_dataframe, hourly_dataframe
 
 
@@ -158,90 +166,6 @@ def insert_into_db(daily_dataframe, hourly_dataframe):
     return daily_count, hourly_count
 
 
-# def kelvin_to_celsius(kelvin):
-#     return (kelvin - 273.15)
-#
-#
-# def request_weather():
-#     try:
-#         config = scheduler.app.config
-#         url = f"{config['BASE_URL']}lat={config['LATITUDE']}&lon={config['LONGITUDE']}&appid={config['API_KEY']}"
-#         request = requests.get(url)
-#         scheduler.app.logger.debug(f"Request made to {url}")
-#         json = request.json()
-#         request.raise_for_status()
-#     except Exception as error:
-#         scheduler.app.logger.error(f"A {type(error).__name__} has occured | {request.status_code} {request.reason} | Response {json}")
-#     return request.ok, json
-#
-#
-# def extract_data(json):
-#     try:
-#         organised_forecast = {}
-#         timezone = pytz.timezone(scheduler.app.config["TIMEZONE"])
-#         five_day_forecast = json["list"]
-#         for three_hour_step in five_day_forecast:
-#             date_time = datetime.utcfromtimestamp(three_hour_step["dt"])
-#             weather_data = {
-#                 "time": date_time.time(),
-#                 "temperature_c": round(kelvin_to_celsius(three_hour_step["main"]["temp"]), 2),
-#                 "humidity": three_hour_step["main"]["humidity"],
-#                 "description": three_hour_step["weather"][0]["description"],
-#                 "rain_probability": three_hour_step["pop"]
-#             }
-#             # the volume of rain fall is not always included, check it exists or set it to zero
-#             if "rain" in three_hour_step:
-#                 weather_data["rain_volume_mm"] = three_hour_step["rain"]["3h"]
-#             else:
-#                 weather_data["rain_volume_mm"] = 0
-#             # store all three houlry weather data, sunrise, and sunset for its date in organised forecast
-#             date = date_time.date()
-#             if date in organised_forecast:
-#                 organised_forecast[date]["weather_data"].append(weather_data)
-#             else:
-#                 sun = Sun(scheduler.app.config["LATITUDE"], scheduler.app.config["LONGITUDE"])
-#                 organised_forecast[date] = {"weather_data": [weather_data], "sunrise": sun.get_sunrise_time(date_time, timezone).time(), "sunset": sun.get_sunset_time(date_time, timezone).time()}
-#         return organised_forecast
-#     except Exception as error:
-#         scheduler.app.logger.error(f"A {type(error).__name__} has occured | {traceback.format_exc()}")
-#         return {}
-#
-#
-# def remove_missing(organised_forecast):
-#     # remove entries that have missing data, for some reason openweathermap include a 6th day in a 5 day forecast which has missing data
-#     remove = []
-#     remove_log = []
-#     for date, data in organised_forecast.items():
-#         if len(data["weather_data"]) < 8:
-#             remove.append(date)
-#             remove_log.append(date.strftime("%d/%m/%y"))
-#     for date in remove:
-#         organised_forecast.pop(date)
-#     scheduler.app.logger.debug(f"Dates {remove_log} contain missing data, removing")
-#     return organised_forecast
-#
-#
-# def add_to_db(organised_forecast):
-#     with scheduler.app.app_context():
-#         dates = [day.date for day in Day.query.order_by(Day.date).all()]
-#         add_log = []
-#         exists_log = []
-#         for date, data in organised_forecast.items():
-#             if date not in dates:
-#                 day = Day(date=date, sunrise=data["sunrise"], sunset=data["sunset"])
-#                 add_log.append(date.strftime("%d/%m/%y"))
-#                 for weather_data in data["weather_data"]:
-#                     weather = Weather(time=weather_data["time"], temperature_c=weather_data["temperature_c"], humidity=weather_data["humidity"], description=weather_data["description"], rain_probability=weather_data["rain_probability"], rain_volume_mm=weather_data["rain_volume_mm"])
-#                     day.weather.append(weather)
-#                 db.session.add(day)
-#             else:
-#                 exists_log.append(date.strftime("%d/%m/%y"))
-#         db.session.commit()
-#     scheduler.app.logger.debug(f"Dates {exists_log} already exist in database")
-#     scheduler.app.logger.debug(f"Dates {add_log} added to database")
-#     return add_log
-#
-#
 # @scheduler.task("cron", id="delete_old_records", minute="30", hour="1", day="*", month="*", day_of_week="*")
 # def delete_old_records():
 #     with scheduler.app.app_context():
