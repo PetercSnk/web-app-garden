2ee5b2eea47bcdbd08aaeb2dc1bf4cc33fad090c
Peter Snook <snookp2@cardiff.ac.uk>
2024-08-27 15:16:11 +0100

    renamed app

--- web-application/app.py
+++ web-application/app.py
@@ -2,14 +2,11 @@
 import logging
 from app import create_app
 
-# Creates configured flask application object.
 app = create_app()
 
-# Executes locally if run directly.
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 else:
-    # TODO test gunicorn with linux
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
---
+++ web-application/gunicorn_config.py
@@ -0,0 +1,8 @@
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
+max_requests = 1000
+max_requests_jitter = 50
+errorlog = "-"
+loglevel = "info"
+bind = "0.0.0.0:8000"
