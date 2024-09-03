7c534deb14b2f252f0cbe45cd72b3ab8bbe843d6
Peter Snook <snookp2@cardiff.ac.uk>
2024-09-03 16:58:28 +0100

    add admin page

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -2,8 +2,9 @@ from flask import render_template, flash, redirect, url_for, current_app
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import check_password_hash
 from app.auth.models import User
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, AdminForm
 from app.auth import auth_bp
+from app import db
 
 
 @auth_bp.route("/login", methods=["GET", "POST"])
@@ -37,3 +38,16 @@ def logout():
     logout_user()
     flash("Logged Out", category="success")
     return redirect(url_for("auth_bp.login"))
+
+
+@auth_bp.route("/admin")
+@login_required
+def admin():
+    admin_form = AdminForm()
+    if admin_form.validate_on_submit():
+        user = User.query.filter_by(username=current_user).first()
+        user.username = admin_form.username.data
+        user.password = admin_form.password.data
+        db.session.commit()
+        current_app.logger.info("User account updated")
+    pass
--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,3 +9,10 @@ class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
+
+
+class AdminForm(FlaskForm):
+    """Form to update users details."""
+    username = StringField("Username")
+    password = PasswordField("Password")
+    submit = SubmitField("Update")
