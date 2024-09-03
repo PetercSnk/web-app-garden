80c61fbcb690d89194e754fc768bd044f0eefa36
Peter Snook <snookp2@cardiff.ac.uk>
2024-09-03 15:20:51 +0100

    db's removed from instance folders
    sqlalchemy doesnt like it when instance folder is missing and cant
    upload empty folder to git

--- app/core/config.py
+++ app/core/config.py
@@ -6,11 +6,11 @@ basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 class Config(object):
     SECRET_KEY = os.urandom(12).hex()
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "core", "instance", "default.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "core", "default.db")
     SQLALCHEMY_BINDS = {
-        "users": "sqlite:///" + os.path.join(basedir, "auth", "instance", "users.db"),
-        "weather": "sqlite:///" + os.path.join(basedir, "weather", "instance", "weather.db"),
-        "water": "sqlite:///" + os.path.join(basedir, "water", "instance", "water.db")
+        "users": "sqlite:///" + os.path.join(basedir, "auth", "users.db"),
+        "weather": "sqlite:///" + os.path.join(basedir, "weather", "weather.db"),
+        "water": "sqlite:///" + os.path.join(basedir, "water", "water.db")
     }
     SCHEDULER_API_ENABLED = True
     URL = "https://api.open-meteo.com/v1/forecast"
