d56768c290a06a34771cca6a36fcdfc26fa872ee
Peter Snook <snookp2@cardiff.ac.uk>
2024-09-04 16:34:54 +0100

    put forms back to old format

--- app/water/forms.py
+++ app/water/forms.py
@@ -10,15 +10,9 @@ class PlantForm(FlaskForm):
     The select field for systems contains only classes that
     are specified within the systems.py file.
     """
-    name = StringField("Name",
-                       validators=[DataRequired(),
-                                   Length(max=10)])
-    description = StringField("Description",
-                              validators=[DataRequired(),
-                                          Length(max=50)])
-    system = SelectField("System",
-                         coerce=int,
-                         validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired(), Length(max=10)])
+    description = StringField("Description", validators=[DataRequired(), Length(max=50)])
+    system = SelectField("System", coerce=int, validators=[DataRequired()])
     submit = SubmitField("Create")
 
 
@@ -27,32 +21,17 @@ class WaterForm(FlaskForm):
 
     This is used for the manual watering of plants.
     """
-    duration_sec = IntegerField("Time (Seconds)",
-                                validators=[DataRequired(),
-                                            NumberRange(min=30,
-                                                        max=300,
-                                                        message="Minimum: %(min)s, Maximum: %(max)s")])
+    duration_sec = IntegerField("Time (Seconds)", validators=[DataRequired(), NumberRange(min=30, max=300, message="Minimum: %(min)s, Maximum: %(max)s")])
     submit = SubmitField("Water")
 
 
 class ConfigForm(FlaskForm):
     """Form to retreive new configs for existing plants."""
     enabled = BooleanField("Enable Automatic Watering")
-    duration_sec = IntegerField("Water Duration (Seconds)",
-                                validators=[DataRequired(),
-                                            NumberRange(min=30,
-                                                        max=300,
-                                                        message="Minimum: %(min)s, Maximum: %(max)s")])
-    occurrence_days = IntegerField("Occurrence (Days)",
-                                   validators=[DataRequired(),
-                                               NumberRange(min=1)])
-    mode = SelectField("Mode",
-                       choices=[(1, "Sunset"), (2, "Sunrise"), (3, "Default")],
-                       coerce=int,
-                       validators=[DataRequired()])
-    default = TimeField("Default Water Time",
-                        validators=[DataRequired()])
+    duration_sec = IntegerField("Water Duration (Seconds)", validators=[DataRequired(), NumberRange(min=30, max=300, message="Minimum: %(min)s, Maximum: %(max)s")])
+    occurrence_days = IntegerField("Occurrence (Days)", validators=[DataRequired(), NumberRange(min=1)])
+    mode = SelectField("Mode", choices=[(1, "Sunset"), (2, "Sunrise"), (3, "Default")], coerce=int, validators=[DataRequired()])
+    default = TimeField("Default Water Time", validators=[DataRequired()])
     rain_reset = BooleanField("Postpone on Rainfall")
-    threshold_mm = IntegerField("Volume of Rainfall Threshold (Millimetres)",
-                                validators=[DataRequired()])
+    threshold_mm = IntegerField("Volume of Rainfall Threshold (Millimetres)", validators=[DataRequired()])
     submit = SubmitField("Save")
