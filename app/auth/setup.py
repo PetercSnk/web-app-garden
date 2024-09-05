139e03f31a6f859163fc80408027588650ae710d
Peter Snook <snookp2@cardiff.ac.uk>
2024-09-05 12:39:15 +0100

    added docstrings to auth startup function

--- app/auth/setup.py
+++ app/auth/setup.py
@@ -1,9 +1,14 @@
+"""Functions that are required to execute on startup for the auth module."""
 from app import db
 from app.auth.models import User
 from werkzeug.security import generate_password_hash
 
 
 def init_accounts():
+    """Creates admin accout if no accounts exist.
+
+    Run this function on startup.
+    """
     users = User.query.all()
     if not users:
         username = "admin"
@@ -11,3 +16,4 @@ def init_accounts():
         default_account = User(username=username, password=password)
         db.session.add(default_account)
         db.session.commit()
+    return
