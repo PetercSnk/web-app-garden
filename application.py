e1efdbff8869872034a40ac44cdcda9c8e3f3957
Peter Snook <snookp2@cardiff.ac.uk>
2024-09-05 14:33:41 +0100

    add logs directory when running application directly

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
 """Executes application."""
+import os
 import logging
 from app import create_app
+
+# Create logs directory before building application when running directly
+if __name__ == "__main__":
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    logsdir = os.path.join(basedir, "logs")
+    if not os.path.exists(logsdir):
+        os.makedirs(logsdir)
 
 app = create_app()
 
