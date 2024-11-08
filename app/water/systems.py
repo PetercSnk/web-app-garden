a78805fcb816473d869b01381257c3f60a744988
Peter Snook <snookp2@cardiff.ac.uk>
2024-11-08 16:28:25 +0000

    combined valve and pump

--- app/water/systems.py
+++ app/water/systems.py
@@ -5,23 +5,92 @@ These methods will be called during the watering process so make sure
 everything is as mentioned. In the case where multiple systems or devices
 are used for one plant combine them into a single class.
 """
-from app.water.valve import Valve
-from app.water.pump import Pump
+import RPi.GPIO as GPIO
 import time
 
 
-class ValvePump(Valve, Pump):
+class ValvePump:
+    """Container class for valve and pump."""
+
     def __init__(self):
-        Valve.__init__(self, 18, 12)
-        Pump.__init__(self, 16)
+        self.valve = self.Valve()
+        self.pump = self.Pump()
         self.wait = 2
 
     def on(self):
-        Valve.on()
+        self.valve.on()
         time.sleep(self.wait)
-        Pump.on()
+        self.pump.on()
 
     def off(self):
-        Valve.off()
+        self.valve.on()
         time.sleep(self.wait)
-        Pump.off()
+        self.pump.on()
+
+    class Valve:
+        """Controls outdoor valve.
+
+        The input readings for the switch are not stable
+        and often look like 000010101101111. Thus, the
+        following methods deal with this and avoid leaving
+        the valve half open.
+        """
+
+        def __init__(self):
+            self.relay = 18
+            self.switch = 12
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.relay, GPIO.OUT)
+            GPIO.setup(self.switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        def on(self):
+            high = False
+            low = False
+            GPIO.output(self.relay, True)
+            while True:
+                i = GPIO.input(self.switch)
+                if (i == 0):
+                    low = True
+                    time.sleep(0.1)
+                elif (i == 1):
+                    high = True
+                    time.sleep(0.1)
+                if high and not low:
+                    high = False
+                elif high and low:
+                    GPIO.output(self.relay, False)
+                    return
+
+        def off(self):
+            high = False
+            low = False
+            GPIO.output(self.relay, True)
+            while True:
+                i = GPIO.input(self.switch)
+                if (i == 0):
+                    low = True
+                    time.sleep(0.1)
+                elif (i == 1):
+                    high = True
+                    time.sleep(0.1)
+                if low and not high:
+                    low = False
+                elif high and low:
+                    GPIO.output(self.relay, False)
+                    return
+
+    class Pump:
+        """Controls outdoor pump."""
+
+        def __init__(self):
+            self.relay = 16
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(self.relay, GPIO.OUT)
+
+        def on(self):
+            GPIO.output(self.relay, True)
+            return
+
+        def off(self):
+            GPIO.output(self.relay, False)
+            return
